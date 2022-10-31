90741bcab3e14e653ff9e80db863531d167defc3
RaThida <100819767+RaThida@users.noreply.github.com>
2022-10-31 08:31:11 +0700

    admin

---
+++ home.php
@@ -0,0 +1,40 @@
+
+	<h1 class="h1">Welcome to <?php echo $_settings->info('name') ?></h1>
+	<hr>
+	<?php
+	$sched_arr=array();
+	?>
+
+	<link rel="stylesheet" href="<?php echo base_url ?>assets/css/home.css">
+
+	<div class="row">
+		<div class="col-lg-3 col-6">
+		<!-- small box -->
+			<div class="small-box bg-info">
+				<div class="inner">
+				 <h3><?php echo $conn->query("SELECT * FROM `assembly_hall` ")->num_rows; ?></h3>
+
+					<p>Total Hall/Room</p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-door-open"></i>
+				</div>
+				<a href="./?page=assembly_hall" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+			<!-- ./col -->
+		<div class="col-lg-3 col-6">
+			<!-- small box -->
+			<div class="small-box bg-success">
+				<div class="inner">
+					<h3><?php echo $conn->query("SELECT * FROM `schedule_list` where date(datetime_start) >= '".date("Y-m-d")."' ")->num_rows; ?></h3>
+
+					<p>Total Reservations</p>
+				</div>
+				<div class="icon">
+					<i class="fa fa-calendar-week"></i>
+				</div>
+				<a href="./?page=schedules" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+	</div>
